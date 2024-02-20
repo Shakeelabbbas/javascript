e6d455126ab2459b6afd720bacec70c63a811148
shakeel Abbas <155940768+Shakeelabbbas@users.noreply.github.com>
2024-02-20 14:17:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+
+ count-=1;
+
+ document.getElementById("Countlabel").innerHTML = count;
+
+}
+
+document.getElementById("resetBtn").onclick = function(){
+     count=0;
+
+ document.getElementById("Countlabel").innerHTML = count;
+ 
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count+=1;
+
+    document.getElementById("Countlabel").innerHTML = count;
+    
+}
